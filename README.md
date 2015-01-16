a97bd9b99eb0ab7c23a653f27e1c1a8d50e55d1f
Richard Bann <richard.bann@vertis.com>
2015-01-16 08:48:10 +0100

    prompt.py, 256-colors, readme added

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # gitprompt
-A simple python script to indicate current git branch and status in bash prompt
+A simple `python` script to indicate current `git` branch and status in
+`bash` prompt.
+
+## Install
+Copy the script (`prompt.py`) into your home directory and put
+the following into `.bashrc`:
+
+```bash
+set_bash_prompt(){
+    PS1="$(python ~/prompt.py)"
+}
+
+PROMPT_COMMAND=set_bash_prompt
+```
+
+## Disclaimer
+The script assumes 256 color terminal, so modification to color codes may be
+needed.
+Find additional help on terminal coloring: http://misc.flogisoft.com/bash/tip_colors_and_formatting
---
+++ prompt.py
@@ -0,0 +1,53 @@
+import subprocess
+
+
+def make_fmt(fg_color=None, bg_color=None,
+             bold=False, italic=False, underline=False):
+
+    _bold = [1] if bold else []
+    _italic = [3] if italic else []
+    _underline = [4] if underline else []
+
+    _bg_color = [] if bg_color is None else [48, 5, bg_color]
+    _fg_color = [] if fg_color is None else [38, 5, fg_color]
+
+    fmt = sum([_bold, _italic, _underline, _bg_color, _fg_color], [])
+    fmt = ';'.join(map(str, fmt))
+
+    return fmt
+
+
+user_fmt = make_fmt(66, bold=True)
+host_fmt = make_fmt(144, bold=True)
+pwd_fmt = make_fmt(215, bold=True)
+clean_fmt = make_fmt(40, bold=True, italic=True)
+dirty_fmt = make_fmt(9, bold=True, italic=True)
+
+
+def colorize(fmt, str):
+    return '\[\x1b[%sm\]%s\[\x1b[0m\]' % (fmt, str)
+
+
+def branch():
+    try:
+        out = subprocess.check_output(['git', 'status'],
+                                      stderr=subprocess.STDOUT)
+    except:
+        return ''
+
+    lines = out.splitlines()
+    branch = lines[0][10:]
+
+    if lines[-1] == 'nothing to commit, working directory clean':
+        branch_fmt = clean_fmt
+    else:
+        branch_fmt = dirty_fmt
+
+    return ' (%s)' % colorize(branch_fmt, branch)
+
+
+user = colorize(user_fmt, '\u')
+host = colorize(host_fmt, '\h')
+pwd = colorize(pwd_fmt, '\w')
+
+print '%s:%s%s $ ' % (user, pwd, branch())
